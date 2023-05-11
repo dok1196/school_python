7dabb979d0186c784af2aadf74250cee84a46e61
dp.shpakov <dp.shpakov@tensor.ru>
2023-05-11 14:09:25 +0700

    Все задания. Проверку проходят 1-6, с 7 проблемы

--- lesson 4/task4.py
+++ lesson 4/task4.py
@@ -8,7 +8,6 @@
 # 4 --> 0 (4 уже одна цифра, а значит мы проделали 0 итераций)
 
 def multiplication_chain(num):
-    # Здесь нужно написать код
     count_multy = 0
     while num >= 10:
         product = 1
