c667146677a267d414534ff0548e09600faf49f8
dok1196 <dok1196@yandex.ru>
2023-06-08 12:42:58 +0700

    10 задание

--- lesson_10/task_2.py
+++ lesson_10/task_2.py
@@ -15,3 +15,25 @@ def all_division(*arg1):
     for i in arg1[1:]:
         division /= i
     return division
+
+@pytest.mark.acceptance
+def test1():
+    assert all_division(10, 2, 5) == 1.0
+
+
+def test2():
+    assert all_division(100, 10, 2, 5) == 1.0
+
+@pytest.mark.acceptance
+def test3():
+    assert all_division(1, 1, 1, 1, 1) == 1.0
+
+
+@pytest.mark.smoke
+def test4():
+    assert all_division(10, 2, 0) == float('inf')
+
+@pytest.mark.smoke
+@pytest.mark.acceptance
+def test5():
+    assert all_division(0, 10, 2) == 0.0
